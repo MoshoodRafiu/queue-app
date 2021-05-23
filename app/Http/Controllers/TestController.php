b5a02544514c99c232f116a318c48b8b7434fb0c
devKunle <meezyloaded@gmail.com>
2021-05-23 07:14:44 +0100

    Set up route and controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\TestJob;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function testJob() :string
+    {
+        foreach (User::all() as $user){
+            TestJob::dispatch($user);
+        }
+        return "Done";
+    }
 }
